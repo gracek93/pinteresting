f3057bd9f93b8186929a54660c94294c89753b4f
Grace Kelly <graciekelly1993@gmail.com>
2015-08-11 19:39:16 +0100

    Added Paperclip and Images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+	
+	#creates two versions of image
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
