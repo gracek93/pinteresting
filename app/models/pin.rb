a37d2d9624aa433ea6d26da09dbb3c2325710814
Grace Kelly <graciekelly1993@gmail.com>
2015-08-13 09:43:45 +0100

    Added name column to users table and added validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,6 @@ class Pin < ActiveRecord::Base
 	#creates two versions of image
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	validates :image, presence: true
 end
